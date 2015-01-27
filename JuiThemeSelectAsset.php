a81cf38aa8046b75242ce884204cc3948965d3eb
Said Bakr <said.fox@gmail.com>
2015-01-27 03:02:52 +0200

    Create JuiThemeSelectAsset.php
    Adding the usage of Yii2 AssetBundle to maintain client-side javascripts needed by the widget.

---
+++ JuiThemeSelectAsset.php
@@ -0,0 +1,30 @@
+<?php
+namespace vendor\saidbakr;
+use yii\web\AssetBundle;
+
+/*
+ * It is free for use and modify with one simple rule:
+ * regarding credits for the author either it modified or not
+ * Author: Said Bakr. said_fox@yahoo.com
+ * http://2index.net
+ */
+
+/**
+ * Description of Kabb
+ *
+ * @author Said
+ */
+class JuiThemeSelectAsset extends AssetBundle
+{
+  public $sourcePath = '@vendor/saidbakr';
+    
+    public $autoGenerate = true;
+    /**
+     * @inheritdoc
+     */
+	public $js = ['jquery.cookie.js','JuiThemeSelect.js'];
+    public $depends = [
+        'yii\jui\JuiAsset',
+    ];
+}
+
